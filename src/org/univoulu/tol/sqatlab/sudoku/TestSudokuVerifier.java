265e22fa2933953a694e8ccfe9ffae5f0e4e049a
somename <someemail>
2015-10-02 13:37:25 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 public class TestSudokuVerifier {
 
+	String inValidStringTest;
 	String inValidStringTest1;
 	String inValidStringTest2;
 	String inValidStringTest3;
@@ -18,6 +19,7 @@ public class TestSudokuVerifier {
     public void initialize() {
     	sv = new SudokuVerifier();
     	validString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+    	inValidStringTest = "3456789912345678891234567789123456678912345567891234456789123345678912234567891";
     	inValidStringTest1 = "003456789912345678891234567789123456678912345567891234456789123345678912234567891";
     	inValidStringTest2 = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
     	inValidStringTest3 = "113456789912345678891234567789123456678912345567891234456789123345678912234567891";
@@ -27,6 +29,14 @@ public class TestSudokuVerifier {
     @Test
     public void testIfSudokuIsValid(){
 		assertEquals(0, sv.verify(validString));
+    }
+    
+    @Test
+    public void testLenghtCorrect(){
+    	//invalid
+    	assertEquals(1, sv.verify(inValidStringTest));
+    	//valid
+    	assertEquals(0, sv.verify(validString));
     }
     
 	@Test
