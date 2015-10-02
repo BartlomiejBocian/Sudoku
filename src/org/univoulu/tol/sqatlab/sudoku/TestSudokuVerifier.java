029f0641eab37002b3184c91189b77ab8f793797
somename <someemail>
2015-10-02 12:38:11 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -9,10 +9,11 @@ public class TestSudokuVerifier {
 
 	String candidateSolution;
 	SudokuVerifier sv;
+	
     @Before 
     public void initialize() {
     	sv = new SudokuVerifier();
-    	candidateSolution= "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+    	candidateSolution= "023456789912345678891234567789123456678912345567891234456789123345678912234567891";
     }
     
     @Test
@@ -23,26 +24,11 @@ public class TestSudokuVerifier {
 		assertEquals(0, sv.verify(candidateSolution));
     }
     
-    @Test
-    public void testVerifyPositveDigits(){
-    	
-    	assertEquals(1, sv.verifyPositveDigits(candidateSolution));
-    	assertEquals(-1, sv.verifyPositveDigits("0rvwsv0000-1"));
-    }
-    
 	@Test
 	public void testFirstRule() {
 		// Act
 		
 		//Assert
 		assertEquals(-1, sv.verify(candidateSolution));
-	}
-
-	@Test
-	public void testLenghtMethod() {
-		// Act
-		
-		//Assert
-		assertEquals(true, sv.validateStringLenght(candidateSolution));
 	}
 }
