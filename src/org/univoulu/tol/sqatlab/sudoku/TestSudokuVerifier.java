2bf757a53f009e80907a70ff9c0a80ec30c9d7bc
somename <someemail>
2015-10-02 12:46:19 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -8,12 +8,14 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 
 	String inValidString;
+	String validString;
 	SudokuVerifier sv;
 	
     @Before 
     public void initialize() {
     	sv = new SudokuVerifier();
-    	inValidString= "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+    	validString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+    	inValidString = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
     }
     
     @Test
