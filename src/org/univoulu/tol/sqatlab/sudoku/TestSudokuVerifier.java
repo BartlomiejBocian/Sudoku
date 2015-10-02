fe17e6630824a4e88da1d8f34aec487c7a543605
somename <someemail>
2015-10-02 13:27:06 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -53,7 +53,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void testGlobalRowMethod(){
 		assertEquals(true, sv.checkDigitInGlobalRow(validString));
-		assertEquals(false, sv.checkDigitInGlobalRow(inValidString));
+		assertEquals(false, sv.checkDigitInGlobalRow(inValidStringTest3));
 	}
 	
 }
