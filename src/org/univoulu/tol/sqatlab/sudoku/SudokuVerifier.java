48427a419f535c5ea47a344ffa9c490362c35a6d
somename <someemail>
2015-10-02 12:23:06 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -6,4 +6,13 @@ public class SudokuVerifier {
 		// returns 1 if the candidate solution is correct
 		return 0;
 	}
+	
+	private int verifyPositveDigits(String string){
+		if (string.matches("[1-9.]+")){
+			return 1;
+		}else{
+			return -1;
+		}
+	}
+	
 }
