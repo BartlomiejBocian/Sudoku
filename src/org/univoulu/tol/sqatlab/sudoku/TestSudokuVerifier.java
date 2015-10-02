7ac9675448170e9444f2140f3729cdcffa3c663c
somename <someemail>
2015-10-02 13:26:47 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 
 public class TestSudokuVerifier {
 
-	String inValidString;
+	String inValidStringTest1;
+	String inValidStringTest2;
+	String inValidStringTest3;
+
 	String validString;
 	SudokuVerifier sv;
 	
@@ -15,31 +18,34 @@ public class TestSudokuVerifier {
     public void initialize() {
     	sv = new SudokuVerifier();
     	validString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-    	inValidString = "113456789912345678891234567789123456678912345567891234456789123345678912234567891";
+    	inValidStringTest1 = "003456789912345678891234567789123456678912345567891234456789123345678912234567891";
+    	inValidStringTest2 = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+    	inValidStringTest3 = "113456789912345678891234567789123456678912345567891234456789123345678912234567891";
+
     }
     
     @Test
     public void testIfSudokuIsValid(){
-		assertEquals(0, sv.verify(inValidString));
+		assertEquals(0, sv.verify(validString));
     }
     
 	@Test
 	public void testFirstRule() {
 		//invalid
-		assertEquals(-1, sv.verify(inValidString));
+		assertEquals(-1, sv.verify(inValidStringTest1));
 		//valid
 		assertEquals(0, sv.verify(validString));
 	}
 	
 	@Test
 	public void testSecondRule() {
-		assertEquals(-2, sv.verify(inValidString));
+		assertEquals(-2, sv.verify(inValidStringTest2));
 	}
 	
 	@Test
 	public void testThirdRule() {
 		//invalid
-		assertEquals(-3, sv.verify(inValidString));
+		assertEquals(-3, sv.verify(inValidStringTest3));
 		//valid
 		assertEquals(0, sv.verify(validString));
 	}
