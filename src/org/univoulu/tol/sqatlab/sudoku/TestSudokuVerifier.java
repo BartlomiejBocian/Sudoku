c05c6878e37efb75a499eb68562d899153069127
somename <someemail>
2015-10-07 23:40:31 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -15,31 +15,31 @@ public class TestSudokuVerifier {
 
 	String validString;
 	SudokuVerifier sv;
-	
-    @Before 
-    public void initialize() {
-    	sv = new SudokuVerifier();
-    	validString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-    	inValidStringTest = "3456789912345678891234567789123456678912345567891234456789123345678912234567891";
-    	inValidStringTest1 = "003456789912345678891234567789123456678912345567891234456789123345678912234567891";
-    	inValidStringTest2 = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
-    	inValidStringTest3 = "117369825632158947958724316825437169791586432346912758289643571573291684164875293";
-    	inValidStringTest4 = "417369825432158947958724316825437169791586432346912758289643571573291684164875293";
-    }
-    
-    @Test
-    public void testIfSudokuIsValid(){
+
+	@Before 
+	public void initialize() {
+		sv = new SudokuVerifier();
+		validString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		inValidStringTest = "3456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		inValidStringTest1 = "003456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		inValidStringTest2 = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		inValidStringTest3 = "117369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		inValidStringTest4 = "417369825432158947958724316825437169791586432346912758289643571573291684164875293";
+	}
+
+	@Test
+	public void testIfSudokuIsValid(){
 		assertEquals(0, sv.verify(validString));
-    }
-    
-    @Test
-    public void testLenghtCorrect(){
-    	//invalid
-    	assertEquals(1, sv.verify(inValidStringTest));
-    	//valid
-    	assertEquals(0, sv.verify(validString));
-    }
-    
+	}
+
+	@Test
+	public void testLenghtCorrect(){
+		//invalid
+		assertEquals(1, sv.verify(inValidStringTest));
+		//valid
+		assertEquals(0, sv.verify(validString));
+	}
+
 	@Test
 	public void testFirstRule() {
 		//invalid
@@ -47,12 +47,12 @@ public class TestSudokuVerifier {
 		//valid
 		assertEquals(0, sv.verify(validString));
 	}
-	
+
 	@Test
 	public void testSecondRule() {
 		assertEquals(-2, sv.verify(inValidStringTest2));
 	}
-	
+
 	@Test
 	public void testThirdRule() {
 		//invalid
@@ -60,7 +60,7 @@ public class TestSudokuVerifier {
 		//valid
 		assertEquals(0, sv.verify(validString));
 	}
-	
+
 	@Test
 	public void testFourthRule() {
 		//invalid
@@ -68,5 +68,5 @@ public class TestSudokuVerifier {
 		//valid
 		assertEquals(0, sv.verify(validString));
 	}
-	
+
 }
