5100381e47fc8bbfb1be069b9640f121f1af010f
somename <someemail>
2015-10-07 22:44:47 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/Sudoku.java
+++ src/org/univoulu/tol/sqatlab/sudoku/Sudoku.java
@@ -1,72 +1,71 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
 public class Sudoku {
-	
-  private String puzzleString;
-	
-  private int[][] myPuzzle = new int[][]{
-		
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	private String puzzleString;
 
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-  };
+	private int[][] myPuzzle = new int[][]{
 
-  public Sudoku(String incomingPuzzle){
-	puzzleString = incomingPuzzle;
-    
-  }
-  
-  public void createPuzzle(){
-	  for(int row = 0; row <= 8; row++){
-	      for(int column = 0; column <= 8; column++){
-	    	  char digits = puzzleString.charAt(row*9+column);
-	        myPuzzle[row][column] = Character.getNumericValue(digits);
-	      }
-	    } 
-  }
-  
-  public boolean validateStringLenght(){
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+	};
+
+	public Sudoku(String incomingPuzzle){
+		puzzleString = incomingPuzzle;
+	}
+
+	public void createPuzzle(){
+		for(int row = 0; row <= 8; row++){
+			for(int column = 0; column <= 8; column++){
+				char digits = puzzleString.charAt(row*9+column);
+				myPuzzle[row][column] = Character.getNumericValue(digits);
+			}
+		} 
+	}
+
+	public boolean validateStringLenght(){
 		if(puzzleString.length() == 81)
 			return true;
 		else {
 			return false;
 		}
-  }
-  
-  public boolean verifyPositveDigits(){
-	  boolean positveDigits = false;
-	  for(int row = 0; row <= 8; row++){
-	      for(int column = 0; column <= 8; column++){
-	        if ( myPuzzle[row][column] >= 1 && myPuzzle[row][column] <= 9){
-	        	positveDigits = true;
-	        }else{
-	        	return false;
-	        }
-	      }
-	    } 
-	return positveDigits;
-  }
-  
-  private void printPuzzle(){
-    for(char y = 0; y <=8; y++){
-      for(char x = 0; x <=8; x++){
-        System.out.print(myPuzzle[y][x] + " ");
-          if ( x == 2 || x == 5 ){
-            System.out.print("  ");
-          }
-      }
-      System.out.println();
-      if ( y == 2 || y == 5 || y == 8 ){
-        System.out.println();
-      }
-    }
-  }
+	}
+
+	public boolean verifyPositveDigits(){
+		boolean positveDigits = false;
+		for(int row = 0; row <= 8; row++){
+			for(int column = 0; column <= 8; column++){
+				if ( myPuzzle[row][column] >= 1 && myPuzzle[row][column] <= 9){
+					positveDigits = true;
+				}else{
+					return false;
+				}
+			}
+		} 
+		return positveDigits;
+	}
+
+	private void printPuzzle(){
+		for(char y = 0; y <=8; y++){
+			for(char x = 0; x <=8; x++){
+				System.out.print(myPuzzle[y][x] + " ");
+				if ( x == 2 || x == 5 ){
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+			if ( y == 2 || y == 5 || y == 8 ){
+				System.out.println();
+			}
+		}
+	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class SudokuVerifier {
 
 	private Sudoku sudoku;
-	
+
 	public int verify(String candidateSolution) {
 		sudoku = new Sudoku(candidateSolution);
 		if(sudoku.validateStringLenght()){
@@ -19,28 +19,20 @@ public class SudokuVerifier {
 			}
 		}else
 			return 1;
-		
+
 		return 0;
 	}
-	
-//	private boolean validateStringLenght(String string){
-//		if(string.length() == 81)
-//			return true;
-//		else {
-//			return false;
-//		}
-//	}
-	
+
 	private boolean checkDigitInGlobalRow(String string){
 		for (String strTmp : string.split("(?<=\\G.{9})")) {
 			Map<Character,Integer> map = new HashMap<Character,Integer>();
 			for (int i = 0; i < strTmp.length(); i++) {
 				char c = strTmp.charAt(i);
-				  if (map.containsKey(c)) {
-				    return false;
-				  } else {
-				    map.put(c, 1);
-				  }
+				if (map.containsKey(c)) {
+					return false;
+				} else {
+					map.put(c, 1);
+				}
 			}
 		}
 		return true;
