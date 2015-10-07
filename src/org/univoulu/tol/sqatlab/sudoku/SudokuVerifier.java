eec7bbb9191b5b9dcbb017a749b90fa71ecf81f1
somename <someemail>
2015-10-07 22:35:53 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -22,14 +22,6 @@ public class SudokuVerifier {
 		return 0;
 	}
 	
-//	private boolean verifyPositveDigits(String string){
-//		if (string.matches("[1-9.]+")){
-//			return true;
-//		}else{
-//			return false;
-//		}
-//	}
-	
 	private boolean validateStringLenght(String string){
 		if(string.length() == 81)
 			return true;
